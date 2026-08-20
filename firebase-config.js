78ace97f316b8940434e6204252fb19d90a04b37
gvssmark <83894487+gvssmark@users.noreply.github.com>
2026-08-20 14:42:34 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,11 +4,11 @@
 // in client-side code. Access is actually controlled by the security rules
 // you set in Realtime Database -> Rules (see database.rules.json in this repo).
 window.FIREBASE_CONFIG = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://PASTE_YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "PASTE_YOUR_PROJECT",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyAvFoehPeDsk9hWRhB8MKCk0bLQP9V4OQ0",
+  authDomain: "family-holdem.firebaseapp.com",
+  databaseURL: "https://family-holdem-default-rtdb.firebaseio.com",
+  projectId: "family-holdem",
+  storageBucket: "family-holdem.firebasestorage.app",
+  messagingSenderId: "756063591124",
+  appId: "1:756063591124:web:a8f74f70479455c26cb941"
 };
