04a825b5dbcf5df02c53b9c2b4984b3075d20cb6
gvssmark <83894487+gvssmark@users.noreply.github.com>
2026-08-22 12:00:12 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // alone and goes straight to the network, since game state must always be
 // live and can't be served from a cache.
 
-const CACHE_NAME = 'family-holdem-shell-v7';
+const CACHE_NAME = 'family-holdem-shell-v6';
 const SHELL_FILES = [
   './',
   './index.html',
